072a36b6964ca621ff23e932e202631c3a14c582
Sona <sona.machkalyan.00@gmail.com>
2023-05-21 16:45:42 +0400

    Added Lazy Singleton Design pattern

--- src/main/java/org/example/design_patterns/singleton/EagerInitializedSingleton.java
+++ src/main/java/org/example/design_patterns/singleton/EagerInitializedSingleton.java
@@ -3,7 +3,6 @@ package org.example.design_patterns.singleton;
 public class EagerInitializedSingleton {
     private static final EagerInitializedSingleton instance = new EagerInitializedSingleton();
 
-    // private constructor to avoid client applications using the constructor
     private EagerInitializedSingleton(){
     }
 
